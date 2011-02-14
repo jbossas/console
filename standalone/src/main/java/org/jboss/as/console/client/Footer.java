0c9eea29a66fc50dbf568cfcd7fd03dcad5bb82a
Heiko Braun <ike.braun@googlemail.com>
2011-02-14 12:17:42 +0100

    added user image to footer. cleanup left & right margins

--- standalone/src/main/java/org/jboss/as/console/client/Footer.java
+++ standalone/src/main/java/org/jboss/as/console/client/Footer.java
@@ -4,6 +4,7 @@ import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.smartgwt.client.types.VerticalAlignment;
+import com.smartgwt.client.widgets.Img;
 import com.smartgwt.client.widgets.Label;
 import com.smartgwt.client.widgets.toolbar.ToolStrip;
 import com.smartgwt.client.widgets.toolbar.ToolStripSeparator;
@@ -37,7 +38,13 @@ public class Footer implements AuthenticationListener {
         toolstrip.setAlign(VerticalAlignment.CENTER);
         toolstrip.setWidth100();
         toolstrip.setMembersMargin(10);
-        toolstrip.setLayoutRightMargin(5);
+        toolstrip.setLayoutRightMargin(15);
+        toolstrip.setLayoutLeftMargin(10);
+
+        Img img = new Img("global/User_16.png");
+        img.setWidth(16);
+        img.setHeight(16);
+        toolstrip.addMember(img);
 
         label = new Label();
         label.setMargin(5);
