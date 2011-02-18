1e91379c6b79979c2c9fa2e80a2ea1b4fd975bd4
Heiko Braun <ike.braun@googlemail.com>
2011-02-18 09:40:46 +0100

    Create hierarchical PlaceRequests from strings

--- standalone/src/main/java/org/jboss/as/console/client/util/Places.java
+++ standalone/src/main/java/org/jboss/as/console/client/util/Places.java
@@ -15,22 +15,29 @@ public class Places {
     {
         List<PlaceRequest> places = new ArrayList<PlaceRequest>();
 
+        // TODO: Tis is currently limited to a a parent/child hierarchy with depth 1
         if(urlString.contains("/"))
         {
-            String token = urlString.split("/")[1];
-            if(token.contains(";"))
+            String[] parentChild = urlString.split("/");
+
+            String parent = parentChild[0];
+            places.add(new PlaceRequest(parent));
+
+            String child = parentChild[1];
+            if(child.contains(";"))
             {
-                String[] split = token.split(";");
-                String place = split[0];
+                String[] split = child.split(";");
+                String childPlace = split[0];
                 String params[] = split[1].split("=");
 
-                places.add(new PlaceRequest(place).with(params[0], params[1]));
+                places.add(new PlaceRequest(childPlace).with(params[0], params[1]));
             }
             else
             {
-                places.add(new PlaceRequest(token));
+                places.add(new PlaceRequest(child));
             }
         }
+
         return places;
     }
 }
