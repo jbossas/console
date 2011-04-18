180df1cae1ce8084ce0a9d47d3bc521d3ed74608
Heiko Braun <ike.braun@googlemail.com>
2011-04-18 15:26:45 +0200

    Cleanup confirmation window

--- standalone/src/main/java/org/jboss/as/console/client/widgets/Feedback.java
+++ standalone/src/main/java/org/jboss/as/console/client/widgets/Feedback.java
@@ -19,8 +19,10 @@
 
 package org.jboss.as.console.client.widgets;
 
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -38,7 +40,7 @@ public class Feedback {
     {
         final DefaultWindow window = new DefaultWindow(title);
 
-        int width = 200;
+        int width = 240;
         int height = (int) (width / DefaultWindow.GOLDEN_RATIO);
 
         window.setWidth(width);
@@ -46,52 +48,32 @@ public class Feedback {
 
         window.setGlassEnabled(true);
 
-        VerticalPanel panel = new VerticalPanel();
+        DockLayoutPanel panel = new DockLayoutPanel(Style.Unit.PX);
         panel.setStyleName("fill-layout-width");
 
         HTML text = new HTML(message);
 
-        DefaultButton ok = new DefaultButton("OK");
-        ok.getElement().setAttribute("style", "width:50px;height:18px");
-        ok.addClickHandler(new ClickHandler() {
+        ClickHandler confirmHandler = new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
                 handler.onConfirmation(true);
                 window.hide();
             }
-        });
+        };
 
-        Label cancel = new Label("Cancel");
-        cancel.setStyleName("html-link");
-        cancel.addClickHandler(new ClickHandler() {
+        ClickHandler cancelHandler = new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
                 handler.onConfirmation(false);
                 window.hide();
             }
-        });
+        };
 
-        HorizontalPanel options = new HorizontalPanel();
-        options.getElement().setAttribute("style", "width:100%");
-
-        HTML spacer = new HTML("&nbsp;");
-        options.add(spacer);
-        //spacer.getElement().getParentElement().setAttribute("width", "100%");
-
-        options.add(ok);
-        options.add(spacer);
-        options.add(cancel);
-        cancel.getElement().getParentElement().setAttribute("style","vertical-align:middle");
-        ok.getElement().getParentElement().setAttribute("style","vertical-align:middle");
-
-        ok.getElement().getParentElement().setAttribute("align", "right");
-        ok.getElement().getParentElement().setAttribute("width", "100%");
-
+        DialogueOptions options = new DialogueOptions("OK", confirmHandler, "Cancel", cancelHandler);
+        options.getElement().setAttribute("style", "margin-bottom:10px;");
+        panel.addSouth(options, 30);
         panel.add(text);
-        panel.add(options);
 
-        //panel.setWidgetBottomHeight(text, 30, Style.Unit.PX, height-60, Style.Unit.PX);
-        //panel.setWidgetBottomHeight(options, 0, Style.Unit.PX, 30, Style.Unit.PX);
 
         window.setWidget(panel);
 
