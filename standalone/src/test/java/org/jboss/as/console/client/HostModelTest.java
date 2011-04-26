ec7819b5ca2fd1b3a2e450188b509fd8503e85bb
Heiko Braun <ike.braun@googlemail.com>
2011-04-26 11:51:03 +0200

    Fix license header

--- standalone/src/test/java/org/jboss/as/console/client/HostModelTest.java
+++ standalone/src/test/java/org/jboss/as/console/client/HostModelTest.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source
- * Copyright <YEAR> Red Hat Inc. and/or its affiliates and other contributors
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
  * as indicated by the @author tags. All rights reserved.
  * See the copyright.txt in the distribution for a
  * full listing of individual contributors.
@@ -21,7 +21,7 @@ package org.jboss.as.console.client;
 
 /*
  * JBoss, Home of Professional Open Source
- * Copyright <YEAR> Red Hat Inc. and/or its affiliates and other contributors
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
  * as indicated by the @author tags. All rights reserved.
  * See the copyright.txt in the distribution for a
  * full listing of individual contributors.
