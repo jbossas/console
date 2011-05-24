24ed04ef820b831822e1b9edd94cd7b025a39ba4
Heiko Braun <ike.braun@googlemail.com>
2011-05-24 17:51:44 +0200

    xa datasource creation in standalone mode

--- gui/src/main/java/org/jboss/as/console/client/shared/subsys/jca/wizard/XADatasourceStep2.java
+++ gui/src/main/java/org/jboss/as/console/client/shared/subsys/jca/wizard/XADatasourceStep2.java
@@ -32,6 +32,7 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.SingleSelectionModel;
+import org.jboss.as.console.client.Console;
 import org.jboss.as.console.client.shared.subsys.jca.model.JDBCDriver;
 import org.jboss.as.console.client.shared.subsys.jca.model.XADataSource;
 import org.jboss.as.console.client.widgets.ComboBox;
@@ -53,6 +54,7 @@ public class XADatasourceStep2 {
     private NewXADatasourceWizard wizard;
     private SingleSelectionModel<JDBCDriver> selectionModel;
     private XADataSource editedEntity;
+    private CellTable<JDBCDriver> table;
 
     public XADatasourceStep2(NewXADatasourceWizard wizard) {
         this.wizard = wizard;
@@ -68,29 +70,39 @@ public class XADatasourceStep2 {
 
         layout.add(new HTML("<h3>Step 2/4: Datasource Class</h3>"));
 
-        ComboBox groupSelection = new ComboBox();
-        Set<String> groupNames = new HashSet<String>(wizard.getDrivers().size());
-        for(JDBCDriver driver : wizard.getDrivers())
-            groupNames.add(driver.getGroup());
-        groupSelection.setValues(groupNames);
-        groupSelection.setItemSelected(0, true);
+        if(!Console.isStandalone())
+        {
+            ComboBox groupSelection = new ComboBox();
+            Set<String> groupNames = new HashSet<String>(wizard.getDrivers().size());
+            for(JDBCDriver driver : wizard.getDrivers())
+                groupNames.add(driver.getGroup());
+            groupSelection.setValues(groupNames);
+            groupSelection.setItemSelected(0, true);
 
-        HorizontalPanel horz = new HorizontalPanel();
-        horz.setStyleName("fill-layout-width");
-        Label label = new HTML("Server Group"+":&nbsp;");
-        label.setStyleName("form-item-title");
-        horz.add(label);
-        Widget selector = groupSelection.asWidget();
-        horz.add(selector);
+            HorizontalPanel horz = new HorizontalPanel();
+            horz.setStyleName("fill-layout-width");
+            Label label = new HTML("Server Group"+":&nbsp;");
+            label.setStyleName("form-item-title");
+            horz.add(label);
+            Widget selector = groupSelection.asWidget();
+            horz.add(selector);
 
-        label.getElement().getParentElement().setAttribute("align", "right");
-        selector.getElement().getParentElement().setAttribute("width", "100%");
-        layout.add(horz);
+            label.getElement().getParentElement().setAttribute("align", "right");
+            selector.getElement().getParentElement().setAttribute("width", "100%");
+            layout.add(horz);
+
+            groupSelection.addValueChangeHandler(new ValueChangeHandler<String>() {
+                @Override
+                public void onValueChange(ValueChangeEvent<String> event) {
+                    filterTable(event.getValue(), getTable());
+                }
+            });
+        }
 
 
         // ---
 
-        final CellTable<JDBCDriver> table = new DefaultCellTable<JDBCDriver>(5);
+        table = new DefaultCellTable<JDBCDriver>(5);
 
         TextColumn<JDBCDriver> nameColumn = new TextColumn<JDBCDriver>() {
             @Override
@@ -113,21 +125,15 @@ public class XADatasourceStep2 {
         table.setSelectionModel(selectionModel);
 
         // filter and select first record
-        filterTable(groupSelection.getSelectedValue(), table);
+        //filterTable(groupSelection.getSelectedValue(), table);
+        if(Console.isStandalone())
+            provisionTable(table);
 
         layout.add(table);
 
         DefaultPager pager = new DefaultPager();
         pager.setDisplay(table);
         layout.add(pager);
-
-        groupSelection.addValueChangeHandler(new ValueChangeHandler<String>() {
-            @Override
-            public void onValueChange(ValueChangeEvent<String> event) {
-                filterTable(event.getValue(), table);
-            }
-        });
-
 
         ClickHandler submitHandler = new ClickHandler() {
             @Override
@@ -168,6 +174,25 @@ public class XADatasourceStep2 {
         return layout;
     }
 
+    private void provisionTable(CellTable<JDBCDriver> table) {
+
+
+        List<JDBCDriver> drivers = wizard.getDrivers();
+
+        table.setRowCount(drivers.size(), true);
+        table.setRowData(drivers);
+
+        // clear selection
+        JDBCDriver selectedDriver = selectionModel.getSelectedObject();
+        if(selectedDriver!=null)
+            selectionModel.setSelected(selectedDriver, false);
+
+        // new default selection
+        if(drivers.size()>0) {
+            selectionModel.setSelected(drivers.get(0), true);
+        }
+    }
+
     private void filterTable(String group, CellTable<JDBCDriver> table) {
 
 
@@ -192,5 +217,9 @@ public class XADatasourceStep2 {
         if(filtered.size()>0) {
             selectionModel.setSelected(filtered.get(0), true);
         }
+    }
+
+    private CellTable<JDBCDriver> getTable() {
+        return table;
     }
 }
