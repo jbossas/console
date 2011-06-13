56b5a60c50f3c148b20ccfe7eb05951b3a8a8889
Heiko Braun <ike.braun@googlemail.com>
2011-06-13 14:03:23 +0200

    disable debug attribute for JVM options

--- gui/src/main/java/org/jboss/as/console/client/shared/jvm/JvmEditor.java
+++ gui/src/main/java/org/jboss/as/console/client/shared/jvm/JvmEditor.java
@@ -112,10 +112,10 @@ public class JvmEditor {
         TextBoxItem nameItem = new TextBoxItem("name", Console.CONSTANTS.common_label_name());
         TextBoxItem heapItem = new TextBoxItem("heapSize", "Heap Size");
         TextBoxItem maxHeapItem = new TextBoxItem("maxHeapSize", "Max Heap Size");
-        CheckBoxItem debugItem = new CheckBoxItem("debugEnabled", "Debug Enabled?");
+        //CheckBoxItem debugItem = new CheckBoxItem("debugEnabled", "Debug Enabled?");
         //TextBoxItem debugOptionsItem = new TextBoxItem("debugOptions", "Debug Options");
 
-        form.setFields(nameItem, heapItem, maxHeapItem, debugItem);
+        form.setFields(nameItem, heapItem, maxHeapItem);
         form.setEnabled(false);
 
         // ---
