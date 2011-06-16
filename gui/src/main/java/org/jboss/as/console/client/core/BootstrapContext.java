24dd8e891e75b82d03a261687d5c4219cfa5398e
Heiko Braun <ike.braun@googlemail.com>
2011-06-16 21:08:41 +0200

    The HTTP API endpoint context was renamed to "/management"

--- gui/src/main/java/org/jboss/as/console/client/core/BootstrapContext.java
+++ gui/src/main/java/org/jboss/as/console/client/core/BootstrapContext.java
@@ -47,11 +47,11 @@ public class BootstrapContext implements ApplicationProperties {
         */
         loadPersistedProperties();
 
-        String domainApi = GWT.isScript() ? getBaseUrl()+"domain-api" : "http://127.0.0.1:8888/app/proxy"; //"http://localhost:9990/domain-api";
+        String domainApi = GWT.isScript() ? getBaseUrl()+"management" : "http://127.0.0.1:8888/app/proxy";
         setProperty(DOMAIN_API, domainApi);
 
 
-        String deploymentApi = GWT.isScript() ? getBaseUrl()+"domain-api/add-content" : "http://127.0.0.1:8888/app/upload";
+        String deploymentApi = GWT.isScript() ? getBaseUrl()+"management/add-content" : "http://127.0.0.1:8888/app/upload";
         setProperty(DEPLOYMENT_API, deploymentApi);
 
         //Log.info("Domain API Endpoint: " + domainApi);
