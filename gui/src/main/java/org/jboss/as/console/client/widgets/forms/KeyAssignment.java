e8db38ee6c2be81e53deb692660400cf5f85f8f2
Heiko Braun <ike.braun@googlemail.com>
2011-09-23 15:50:52 +0200

    missing interface

--- gui/src/main/java/org/jboss/as/console/client/widgets/forms/KeyAssignment.java
+++ gui/src/main/java/org/jboss/as/console/client/widgets/forms/KeyAssignment.java
@@ -4,5 +4,6 @@ package org.jboss.as.console.client.widgets.forms;
  * @author Heiko Braun
  * @date 9/23/11
  */
-public class KeyAssignment {
+public interface KeyAssignment {
+    Object valueForKey(String key);
 }
