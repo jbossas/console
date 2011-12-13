a906c1a8adcd95aaf58181bcfa29ac59d31ef816
Heiko Braun <ike.braun@googlemail.com>
2011-12-13 09:17:06 +0100

    rename resource adapters LHS nav link

--- gui/src/main/java/org/jboss/as/console/client/shared/SubsystemMetaData.java
+++ gui/src/main/java/org/jboss/as/console/client/shared/SubsystemMetaData.java
@@ -69,7 +69,7 @@ public class SubsystemMetaData {
         // assign actual subsystems
         groups.get(CONNECTOR).getItems().add(new SubsystemGroupItem("JCA", "jca"));
         groups.get(CONNECTOR).getItems().add(new SubsystemGroupItem("Datasources", "datasources"));
-        groups.get(CONNECTOR).getItems().add(new SubsystemGroupItem("Resource Adapter", "resource-adapters"));
+        groups.get(CONNECTOR).getItems().add(new SubsystemGroupItem("Resource Adapters", "resource-adapters"));
         groups.get(CONNECTOR).getItems().add(new SubsystemGroupItem("Connector", "connector",Boolean.TRUE));
         groups.get(CONNECTOR).getItems().add(new SubsystemGroupItem("Mail", "mail"));
 
