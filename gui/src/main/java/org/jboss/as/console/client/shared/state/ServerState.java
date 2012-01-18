039035b9c7a8fbb6b021938b43a085e0ff63cc07
Heiko Braun <ike.braun@googlemail.com>
2012-01-18 10:40:35 +0100

    missing class

--- gui/src/main/java/org/jboss/as/console/client/shared/state/ServerState.java
+++ gui/src/main/java/org/jboss/as/console/client/shared/state/ServerState.java
@@ -5,4 +5,31 @@ package org.jboss.as.console.client.shared.state;
  * @date 1/18/12
  */
 public class ServerState {
+    private String name;
+    private boolean reloadRequired;
+    private boolean restartRequired;
+
+    public ServerState(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isReloadRequired() {
+        return reloadRequired;
+    }
+
+    public void setReloadRequired(boolean reloadRequired) {
+        this.reloadRequired = reloadRequired;
+    }
+
+    public boolean isRestartRequired() {
+        return restartRequired;
+    }
+
+    public void setRestartRequired(boolean restartRequired) {
+        this.restartRequired = restartRequired;
+    }
 }
