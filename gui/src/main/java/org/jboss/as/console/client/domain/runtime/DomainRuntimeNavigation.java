3050fd78a04c1f5f664125909034a5ca9cdd7289
Heiko Braun <ike.braun@googlemail.com>
2012-01-19 09:39:53 +0100

    use predicates for runtime navigation entries

--- gui/src/main/java/org/jboss/as/console/client/domain/runtime/DomainRuntimeNavigation.java
+++ gui/src/main/java/org/jboss/as/console/client/domain/runtime/DomainRuntimeNavigation.java
@@ -16,6 +16,7 @@ import org.jboss.ballroom.client.layout.LHSNavTree;
 import org.jboss.ballroom.client.layout.LHSNavTreeItem;
 import org.jboss.ballroom.client.widgets.stack.DisclosureStackPanel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,6 +30,8 @@ class DomainRuntimeNavigation {
 
     private ServerPicker serverPicker;
     private DefaultTreeItem subsystemTree;
+
+    private List<Predicate> potentialItems = new ArrayList<Predicate>();
 
     public Widget asWidget()
     {
@@ -69,6 +72,17 @@ class DomainRuntimeNavigation {
         Tree metricTree = new LHSNavTree("domain-runtime");
 
         subsystemTree = new DefaultTreeItem("Subsystem Metrics");
+
+
+        LHSNavTreeItem datasources = new LHSNavTreeItem("Datasources", "ds-metrics");
+        LHSNavTreeItem jmsQueues = new LHSNavTreeItem("JMS Destinations", "jms-metrics");
+        LHSNavTreeItem web = new LHSNavTreeItem("Web", "web-metrics");
+        LHSNavTreeItem tx = new LHSNavTreeItem("Transactions", "tx-metrics");
+
+        potentialItems.add(new Predicate("datasources", datasources));
+        potentialItems.add(new Predicate("messaging", jmsQueues));
+        potentialItems.add(new Predicate("web", web));
+        potentialItems.add(new Predicate("transactions", tx));
 
         metricTree.addItem(subsystemTree);
         //subsystemTree.setState(true); // open by default
@@ -114,23 +128,35 @@ class DomainRuntimeNavigation {
         subsystemTree.setState(false);
         subsystemTree.removeItems();
 
-
-        System.out.println("subsystems: "+ subsystems.size());
-
-        LHSNavTreeItem datasources = new LHSNavTreeItem("Datasources", "ds-metrics");
-        LHSNavTreeItem jmsQueues = new LHSNavTreeItem("JMS Destinations", "jms-metrics");
-        LHSNavTreeItem web = new LHSNavTreeItem("Web", "web-metrics");
-        LHSNavTreeItem tx = new LHSNavTreeItem("Transactions", "tx-metrics");
-
-        subsystemTree.addItem(datasources);
-        subsystemTree.addItem(jmsQueues);
-        subsystemTree.addItem(web);
-        subsystemTree.addItem(tx);
-
+        // match subystems
+        for(SubsystemRecord subsys : subsystems)
+        {
+            for(Predicate predicate : potentialItems)
+            {
+                if(predicate.matches(subsys.getKey()))
+                    subsystemTree.addItem(predicate.getNavItem());
+            }
+        }
 
         subsystemTree.setState(true);
     }
 
+    final class Predicate {
+        private String subsysName;
+        private LHSNavTreeItem navItem;
 
+        Predicate(String subsysName, LHSNavTreeItem navItem) {
+            this.subsysName = subsysName;
+            this.navItem = navItem;
+        }
+
+        public boolean matches(String current) {
+                return current.equals(subsysName);
+        }
+
+        public LHSNavTreeItem getNavItem() {
+            return navItem;
+        }
+    }
 
 }
