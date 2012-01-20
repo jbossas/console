3d325a373978854954e25e03bbf388c40e80930f
Heiko Braun <ike.braun@googlemail.com>
2012-01-20 08:45:00 +0100

    Provide a chart text column implementation

--- gui/src/main/java/org/jboss/as/console/client/shared/runtime/charts/TextColumn.java
+++ gui/src/main/java/org/jboss/as/console/client/shared/runtime/charts/TextColumn.java
@@ -1,8 +1,20 @@
 package org.jboss.as.console.client.shared.runtime.charts;
 
+import com.google.gwt.visualization.client.AbstractDataTable;
+
 /**
  * @author Heiko Braun
  * @date 1/19/12
  */
-public class TextColumn {
+public class TextColumn extends Column<String> {
+
+    public TextColumn(String detypedName, String label) {
+        super(AbstractDataTable.ColumnType.STRING, label);
+        setDeytpedName(detypedName);
+    }
+
+    @Override
+    String cast(String value) {
+        return value;
+    }
 }
