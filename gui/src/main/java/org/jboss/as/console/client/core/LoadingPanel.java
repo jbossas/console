1a51ef28dcb755826ade2629bb9361f9083eeb37
Heiko Braun <ike.braun@googlemail.com>
2012-02-01 11:07:59 +0100

    Fix the loading panel on IE 7

--- gui/src/main/java/org/jboss/as/console/client/core/LoadingPanel.java
+++ gui/src/main/java/org/jboss/as/console/client/core/LoadingPanel.java
@@ -1,9 +1,6 @@
 package org.jboss.as.console.client.core;
 
-import com.google.gwt.dom.client.Style;
-import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
@@ -14,23 +11,9 @@ public class LoadingPanel {
 
     public Widget asWidget() {
 
-        final LayoutPanel loadingPanel = new LayoutPanel();
-        loadingPanel.setStyleName("loading-panel");
+        HTMLPanel html = new HTMLPanel("<center><div id='loading-panel'><img src='images/loading_lite.gif' style='padding-top:3px;vertical-align:middle'/> Loading ... </div></center>");
 
-        Image loadingImage = new Image("images/loading_lite.gif");
-        Label label = new Label("Loading ...");
-        label.getElement().setAttribute("style", "padding-right:5px;");
-
-        loadingPanel.add(loadingImage);
-        loadingPanel.add(label);
-
-        loadingPanel.setWidgetLeftRight(loadingImage, 5, Style.Unit.PX, 25, Style.Unit.PX);
-        loadingPanel.setWidgetLeftRight(label, 38, Style.Unit.PX, 0, Style.Unit.PX);
-
-        loadingPanel.setWidgetTopHeight(loadingImage, 10, Style.Unit.PX, 25, Style.Unit.PX);
-        loadingPanel.setWidgetTopHeight(label, 15, Style.Unit.PX, 25, Style.Unit.PX);
-
-        return loadingPanel;
+        return html.asWidget();
 
     }
 }
