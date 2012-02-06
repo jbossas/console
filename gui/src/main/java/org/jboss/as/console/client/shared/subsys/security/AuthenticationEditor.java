077677c57fd5cdef6d74203b764c4466e6f1b1c2
David Bosschaert <david.bosschaert@gmail.com>
2012-02-06 07:58:35 +0000

    Line up security domain enumeration with model values.

--- gui/src/main/java/org/jboss/as/console/client/shared/subsys/security/AuthenticationEditor.java
+++ gui/src/main/java/org/jboss/as/console/client/shared/subsys/security/AuthenticationEditor.java
@@ -18,12 +18,12 @@
  */
 package org.jboss.as.console.client.shared.subsys.security;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.jboss.as.console.client.Console;
 import org.jboss.as.console.client.shared.subsys.security.model.AuthenticationLoginModule;
 import org.jboss.as.console.client.shared.subsys.security.wizard.NewAuthPolicyModuleWizard;
-
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * @author David Bosschaert
@@ -56,12 +56,11 @@ public class AuthenticationEditor extends AuthEditor<AuthenticationLoginModule>{
 
     @Override
     Wizard<AuthenticationLoginModule> getWizard() {
-
         List<String> flagValues = new LinkedList<String>();
-        flagValues.add("REQUIRED");
-        flagValues.add("REQUISITE");
-        flagValues.add("SUFFICIENT");
-        flagValues.add("OPTIONAL");
+        flagValues.add("required");
+        flagValues.add("requisite");
+        flagValues.add("sufficient");
+        flagValues.add("optional");
 
         // should really wait until flagValues are set.
         return new NewAuthPolicyModuleWizard<AuthenticationLoginModule>(this, entityClass, flagValues,
