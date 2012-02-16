ba4664667472c4a4f25544e502cd4381bb2d69e1
Heiko Braun <ike.braun@googlemail.com>
2012-02-16 14:51:54 +0100

    operations to add remove protcols

---
+++ gui/src/main/java/org/jboss/as/console/client/shared/subsys/jgroups/NewProtocolWizard.java
@@ -0,0 +1,90 @@
+package org.jboss.as.console.client.shared.subsys.jgroups;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import org.jboss.as.console.client.shared.help.FormHelpPanel;
+import org.jboss.as.console.client.shared.subsys.Baseadress;
+import org.jboss.ballroom.client.widgets.forms.Form;
+import org.jboss.ballroom.client.widgets.forms.FormValidation;
+import org.jboss.ballroom.client.widgets.forms.TextBoxItem;
+import org.jboss.ballroom.client.widgets.forms.TextItem;
+import org.jboss.ballroom.client.widgets.window.DialogueOptions;
+import org.jboss.ballroom.client.widgets.window.WindowContentBuilder;
+import org.jboss.dmr.client.ModelNode;
+
+/**
+ * @author Heiko Braun
+ * @date 2/16/12
+ */
+public class NewProtocolWizard {
+
+
+    private JGroupsPresenter presenter;
+
+    public NewProtocolWizard(JGroupsPresenter presenter) {
+        this.presenter = presenter;
+    }
+
+    Widget asWidget() {
+        VerticalPanel layout = new VerticalPanel();
+        layout.setStyleName("window-content");
+
+        final Form<JGroupsProtocol> form = new Form<JGroupsProtocol>(JGroupsProtocol.class);
+
+        TextBoxItem typeField = new TextBoxItem("type", "Type");
+        TextBoxItem socket = new TextBoxItem("socketBinding", "Socket Binding");
+
+        form.setFields(typeField, socket);
+
+
+        DialogueOptions options = new DialogueOptions(
+
+                // save
+                new ClickHandler() {
+                    @Override
+                    public void onClick(ClickEvent event) {
+                        // merge base
+
+                        FormValidation validation = form.validate();
+                        if(validation.hasErrors())
+                            return;
+
+                        presenter.onCreateProtocol(form.getUpdatedEntity());
+
+                    }
+                },
+
+                // cancel
+                new ClickHandler() {
+                    @Override
+                    public void onClick(ClickEvent event) {
+                        presenter.closeDialoge();
+                    }
+                }
+
+        );
+
+        // ----------------------------------------
+
+        Widget formWidget = form.asWidget();
+
+        FormHelpPanel helpPanel = new FormHelpPanel(new FormHelpPanel.AddressCallback() {
+            @Override
+            public ModelNode getAddress() {
+                ModelNode address = Baseadress.get();
+                address.add("subsystem", "jgroups");
+                address.add("stack", "*");
+                address.add("protocol", "*");
+                return address;
+            }
+        }, form);
+
+        layout.add(helpPanel.asWidget());
+
+        layout.add(formWidget);
+
+        return new WindowContentBuilder(layout, options).build();
+    }
+}
