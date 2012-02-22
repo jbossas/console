4c7a6abcc3850e1db9972e65eb5c573f74c09888
Heiko Braun <ike.braun@googlemail.com>
2012-02-22 14:29:10 +0100

    rename and fix zanata script

---
+++ gui/pull-translations.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# pull from zanata repo
+mvn zanata:pull -Dzanata.projectType=utf8properties
+
+
+
+
+
+
+
--- gui/zanata-pull.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mvn zanata:pull -Dzanta.projectType=utf8properties
-
-
-find . -name "*.properties" | while read FILENAME; do native2ascii $FILENAME $FILENAME; done
