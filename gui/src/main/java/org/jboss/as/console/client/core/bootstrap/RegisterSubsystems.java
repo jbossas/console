c4400e9b37cdba3aaecdf5277f7632c4b0e483b2
Heiko Braun <ike.braun@googlemail.com>
2012-03-27 11:20:40 +0200

    missing class

--- gui/src/main/java/org/jboss/as/console/client/core/bootstrap/RegisterSubsystems.java
+++ gui/src/main/java/org/jboss/as/console/client/core/bootstrap/RegisterSubsystems.java
@@ -1,8 +1,20 @@
 package org.jboss.as.console.client.core.bootstrap;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import org.jboss.as.console.client.Console;
+import org.jboss.as.console.client.shared.SubsystemMetaData;
+import org.jboss.as.console.client.shared.dispatch.AsyncCommand;
+
 /**
  * @author Heiko Braun
  * @date 3/27/12
  */
-public class RegisterSubsystems {
+public class RegisterSubsystems implements AsyncCommand<Boolean>{
+    @Override
+    public void execute(AsyncCallback<Boolean> callback) {
+
+        SubsystemMetaData.bootstrap(Console.MODULES.getSubsystemRegistry());
+
+        callback.onSuccess(Boolean.TRUE);
+    }
 }
