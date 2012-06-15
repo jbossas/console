c259e2e0423285ce35715277583f69c12b2383da
Heiko Braun <ike.braun@googlemail.com>
2012-06-15 13:45:52 +0200

    provide a raw view for the browser

---
+++ gui/src/main/java/org/jboss/as/console/client/tools/RawView.java
@@ -0,0 +1,45 @@
+package org.jboss.as.console.client.tools;
+
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import org.jboss.dmr.client.Property;
+
+/**
+ * @author Heiko Braun
+ * @date 6/15/12
+ */
+public class RawView {
+
+    private TextArea dump;
+    private TextBox name;
+    private TextBox type;
+
+    Widget asWidget() {
+        VerticalPanel layout = new VerticalPanel();
+        layout.setStyleName("fill-layout-width");
+        layout.getElement().setAttribute("style", "padding:10px");
+
+        dump = new TextArea();
+        dump.setVisibleLines(10);
+        dump.setCharacterWidth(200);
+
+
+        name = new TextBox();
+        type = new TextBox();
+
+        layout.add(name);
+        layout.add(type);
+        layout.add(dump);
+
+        return layout;
+    }
+
+    public void display(Property model)
+    {
+        name.setText(model.getName());
+        type.setText(model.getValue().getType().name());
+        dump.setText(model.getValue().toString());
+    }
+}
