09446a301b49dfd01a3b70305ae20ff0b045ad8a
Heiko Braun <ike.braun@googlemail.com>
2012-06-15 10:56:39 +0200

    Provide the ability to filter the environment properties

--- gui/src/main/java/org/jboss/as/console/client/shared/general/EnvironmentProperties.java
+++ gui/src/main/java/org/jboss/as/console/client/shared/general/EnvironmentProperties.java
@@ -2,10 +2,18 @@ package org.jboss.as.console.client.shared.general;
 
 import com.google.gwt.cell.client.SafeHtmlCell;
 import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.ColumnSortEvent;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.ListDataProvider;
@@ -17,6 +25,7 @@ import org.jboss.ballroom.client.widgets.forms.TextAreaItem;
 import org.jboss.ballroom.client.widgets.forms.TextItem;
 import org.jboss.ballroom.client.widgets.tables.DefaultCellTable;
 import org.jboss.ballroom.client.widgets.tables.DefaultPager;
+import org.jboss.ballroom.client.widgets.tools.ToolStrip;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -31,8 +40,9 @@ public class EnvironmentProperties {
 
     private ListDataProvider<PropertyRecord> propertyProvider;
     private DefaultCellTable<PropertyRecord> propertyTable;
+    private List<PropertyRecord> origValues = Collections.EMPTY_LIST;
 
-   public Widget asWidget() {
+    public Widget asWidget() {
         propertyTable = new DefaultCellTable<PropertyRecord>(8, new ProvidesKey<PropertyRecord>() {
             @Override
             public Object getKey(PropertyRecord item) {
@@ -106,6 +116,34 @@ public class EnvironmentProperties {
         VerticalPanel layout = new VerticalPanel();
         layout.setStyleName("fill-layout-width");
 
+
+        final TextBox filter = new TextBox();
+        filter.setMaxLength(30);
+        filter.setVisibleLength(20);
+        filter.getElement().setAttribute("style", "float:right; width:120px;");
+        filter.addKeyDownHandler(new KeyDownHandler() {
+            @Override
+            public void onKeyDown(KeyDownEvent keyDownEvent) {
+                String prefix = filter.getText();
+                if(prefix!=null && !prefix.equals(""))
+                {
+                    // filter by prefix
+                    filterByPrefix(prefix);
+                }
+                else
+                {
+                    clearFilter();
+                }
+            }
+        });
+
+        ToolStrip toolStrip = new ToolStrip();
+        final Label label = new Label("Filter: ");
+        label.getElement().setAttribute("style", "padding-top:8px;");
+        toolStrip.addToolWidgetRight(label);
+        toolStrip.addToolWidgetRight(filter);
+        layout.add(toolStrip.asWidget());
+
         layout.add(propertyTable);
 
         DefaultPager pager = new DefaultPager();
@@ -119,6 +157,8 @@ public class EnvironmentProperties {
     }
 
     public void setProperties(List<PropertyRecord> environment) {
+
+        origValues = environment;
         propertyTable.setRowCount(environment.size(), true);
 
         List<PropertyRecord> propList = propertyProvider.getList();
@@ -136,5 +176,26 @@ public class EnvironmentProperties {
 
     public void clearValues() {
         propertyProvider.setList(new ArrayList<PropertyRecord>());
+    }
+
+    public void filterByPrefix(String prefix) {
+
+        final List<PropertyRecord> next  = new ArrayList<PropertyRecord>();
+        for(PropertyRecord prop : origValues)
+        {
+            if(prop.getKey().startsWith(prefix))
+                next.add(prop);
+        }
+
+        List<PropertyRecord> propList = propertyProvider.getList();
+        propList.clear(); // cannot call setList() as that breaks the sort handler
+        propList.addAll(next);
+
+    }
+
+    public void clearFilter() {
+        List<PropertyRecord> propList = propertyProvider.getList();
+        propList.clear(); // cannot call setList() as that breaks the sort handler
+        propList.addAll(origValues);
     }
 }
