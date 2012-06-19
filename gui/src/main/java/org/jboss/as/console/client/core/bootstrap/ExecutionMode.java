8f8fedd64c9ff79b9800018ab5b371a9373301ac
Heiko Braun <ike.braun@googlemail.com>
2012-06-19 15:26:48 +0200

    rollback the -Duser.lang switch, it breaks the domain mode

--- gui/src/main/java/org/jboss/as/console/client/core/bootstrap/ExecutionMode.java
+++ gui/src/main/java/org/jboss/as/console/client/core/bootstrap/ExecutionMode.java
@@ -21,21 +21,12 @@ package org.jboss.as.console.client.core.bootstrap;
 
 import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import org.jboss.as.console.client.Console;
 import org.jboss.as.console.client.core.BootstrapContext;
-import org.jboss.as.console.client.shared.Preferences;
 import org.jboss.as.console.client.shared.dispatch.AsyncCommand;
 import org.jboss.as.console.client.shared.dispatch.DispatchAsync;
 import org.jboss.as.console.client.shared.dispatch.impl.DMRAction;
 import org.jboss.as.console.client.shared.dispatch.impl.DMRResponse;
-import org.jboss.as.console.client.shared.properties.PropertyRecord;
-import org.jboss.as.console.client.standalone.StandaloneServer;
-import org.jboss.ballroom.client.widgets.window.Feedback;
 import org.jboss.dmr.client.ModelNode;
-import org.jboss.dmr.client.Property;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.jboss.dmr.client.ModelDescriptionConstants.*;
 
@@ -57,29 +48,11 @@ public class ExecutionMode implements AsyncCommand<Boolean>{
     @Override
     public void execute(final AsyncCallback<Boolean> callback) {
 
-
-
-
-        ModelNode operation = new ModelNode();
-        operation.get(OP).set(COMPOSITE);
+        // :read-attribute(name=process-type)
+        final ModelNode operation = new ModelNode();
+        operation.get(OP).set(READ_ATTRIBUTE_OPERATION);
+        operation.get(NAME).set("process-type");
         operation.get(ADDRESS).setEmptyList();
-
-        List<ModelNode> steps = new ArrayList<ModelNode>();
-
-        ModelNode execModeOp = new ModelNode();
-        execModeOp.get(OP).set(READ_ATTRIBUTE_OPERATION);
-        execModeOp.get(NAME).set("process-type");
-        execModeOp.get(ADDRESS).setEmptyList();
-        steps.add(execModeOp);
-
-        ModelNode envProperties = new ModelNode();
-        envProperties.get(OP).set(READ_ATTRIBUTE_OPERATION);
-        envProperties.get(ADDRESS).add("core-service", "platform-mbean");
-        envProperties.get(ADDRESS).add("type", "runtime");
-        envProperties.get(NAME).set("system-properties");
-        steps.add(envProperties);
-
-        operation.get(STEPS).set(steps);
 
         dispatcher.execute(new DMRAction(operation), new AsyncCallback<DMRResponse>() {
             @Override
@@ -94,41 +67,10 @@ public class ExecutionMode implements AsyncCommand<Boolean>{
             public void onSuccess(DMRResponse result) {
 
                 ModelNode response = result.get();
-                List<Property> propertyList = response.get(RESULT).asPropertyList();
-                boolean outcome = true;
+                boolean isServer = response.get(RESULT).asString().equals("Server");
+                bootstrap.setProperty(BootstrapContext.STANDALONE, Boolean.valueOf(isServer).toString());
 
-                for(Property step : propertyList)
-                {
-                    ModelNode stepResult = step.getValue();
-                    if(step.getName().equals("step-1"))
-                    {
-                        boolean isServer = stepResult.get(RESULT).asString().equals("Server");
-                        bootstrap.setProperty(BootstrapContext.STANDALONE, Boolean.valueOf(isServer).toString());
-                    }
-                    else
-                    {
-                        List<Property> properties = stepResult.get(RESULT).asPropertyList();
-                        for(Property prop : properties)
-                        {
-                            if(prop.getName().equals("user.language"))
-                            {
-                                final String preferredLang = prop.getValue().asString();
-                                if(!preferredLang.equals(Preferences.get("as7_ui_locale", "en")))
-                                {
-                                    Preferences.set("as7_ui_locale", preferredLang);
-                                    bootstrap.setlastError(new Throwable(
-                                        "The user.language has changed. Please reload the web interface!"
-                                    ));
-                                    outcome = false;
-                                }
-
-                                break;
-                            }
-                        }
-                    }
-                }
-
-                callback.onSuccess(outcome);
+                callback.onSuccess(Boolean.TRUE);
 
             }
         });
