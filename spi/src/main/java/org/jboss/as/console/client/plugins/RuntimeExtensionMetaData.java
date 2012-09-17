6e43a2d949b51b5cb29b7dc91c4418cc7c5637ac
Heiko Braun <ike.braun@googlemail.com>
2012-09-17 13:06:29 +0200

    enforce file rename

--- spi/src/main/java/org/jboss/as/console/client/plugins/RuntimeExtensionMetaData.java
+++ spi/src/main/java/org/jboss/as/console/client/plugins/RuntimeExtensionMetaData.java
@@ -4,14 +4,14 @@ package org.jboss.as.console.client.plugins;
  * @author Heiko Braun
  * @date 3/26/12
  */
-public class RuntimeExtensionmetaData {
+public class RuntimeExtensionMetaData {
 
     private String token;
     private String name;
     private String group;
     private String key;
 
-    public RuntimeExtensionmetaData(String name, String token, String group, String key) {
+    public RuntimeExtensionMetaData(String name, String token, String group, String key) {
         this.name = name;
         this.token = token;
         this.group = group;
