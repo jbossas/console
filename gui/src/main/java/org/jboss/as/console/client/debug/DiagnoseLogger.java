c4ab11c67569ea017256675b23e5e60fb7a4a5e6
Heiko Braun <ike.braun@googlemail.com>
2012-10-25 15:25:52 +0200

    Integrate dmd invocation tracking with RPC metric listener

--- gui/src/main/java/org/jboss/as/console/client/debug/DiagnoseLogger.java
+++ gui/src/main/java/org/jboss/as/console/client/debug/DiagnoseLogger.java
@@ -5,6 +5,36 @@ package org.jboss.as.console.client.debug;
  * @date 10/25/12
  */
 public class DiagnoseLogger {
+
+    private static final String RPC = "rpc";
+    private static final String RPC_SERIALIZED = "requestSerialized";
+    private static final String RPC_SENT = "requestSent";
+    private static final String RPC_RESPONSE = "responseReceived";
+    private static final String RPC_DESERIALIZED = "responseDeserialized";
+
+    public static native void logRpc(
+            String type, String id, double millis) /*-{
+       $wnd.__gwtStatsEvent({
+         'moduleName' : "dmr-invocation",
+         'subSystem' : "rpc",
+         'evtGroup' : id,
+         'millis' : millis,
+         'type' : type
+       });
+     }-*/;
+
+    public static native void logRpc(
+            String type, String id, double millis, String method) /*-{
+          $wnd.__gwtStatsEvent({
+            'moduleName' : "dmr-invocation",
+            'subSystem' : "rpc",
+            'evtGroup' : id,
+            'millis' : millis,
+            'type' : type,
+            'method' : method
+          });
+        }-*/;
+
     public static native void logEvent(
             String moduleName, String subSystem,
             String eventGroup, double millis, String type) /*-{
@@ -16,4 +46,15 @@ public class DiagnoseLogger {
       'type' : type
     });
   }-*/;
+
+    public static native void logError (String module, double millis, String errMsg) /*-{
+       $wnd.__gwtStatsEvent({
+         'moduleName' : module,
+         'subSystem' : 'error',
+         'evtGroup' : 'error',
+         'millis' : millis,
+         'type' : 'error',
+         'error' : errMsg
+       });
+     }-*/;
 }
