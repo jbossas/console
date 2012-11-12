200f9b7f340897057cd9cad10616abba8acd4c08
Heiko Braun <ike.braun@googlemail.com>
2012-11-12 14:46:43 +0100

    abstract model node form

---
+++ gui/src/main/java/org/jboss/mbui/client/cui/widgets/InspectionContext.java
@@ -0,0 +1,8 @@
+package org.jboss.mbui.client.cui.widgets;
+
+/**
+ * @author Heiko Braun
+ * @date 11/12/12
+ */
+public class InspectionContext {
+}
---
+++ gui/src/main/java/org/jboss/mbui/client/cui/widgets/ModelNodeVisitor.java
@@ -0,0 +1,68 @@
+package org.jboss.mbui.client.cui.widgets;
+
+import org.jboss.dmr.client.ModelNode;
+
+/**
+ * @author Heiko Braun
+ * @date 11/12/12
+ */
+public class ModelNodeVisitor {
+
+    public void visit(ModelNode bean, InspectionContext ctx) {};
+
+    public void endVisit(ModelNode bean, InspectionContext ctx){};
+
+    public boolean visitValueProperty(
+            final String propertyName,
+            final ModelNode value,
+            PropertyContext ctx
+    )
+    {
+        return true;
+    }
+
+    public boolean endVisitValueProperty(
+            final String propertyName,
+            final ModelNode value,
+            PropertyContext ctx
+    )
+    {
+        return true;
+    }
+
+
+    public boolean visitReferenceProperty(
+            String propertyName,
+            ModelNode value,
+            PropertyContext ctx)
+    {
+        return true;
+    }
+
+    public void endVisitReferenceProperty(
+            String propertyName,
+            ModelNode value,
+            PropertyContext ctx
+    )
+    {
+
+    }
+
+    public boolean visitCollectionProperty(
+            String propertyName,
+            final ModelNode value,
+            PropertyContext ctx)
+    {
+        return true;
+    }
+
+    public boolean endVisitCollectionProperty(
+            String propertyName,
+            final ModelNode value,
+            PropertyContext ctx)
+    {
+        return true;
+    }
+
+
+}
