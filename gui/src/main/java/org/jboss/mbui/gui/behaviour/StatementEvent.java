0b0b63a9665d99918ec0361389736621394bf0c5
Heiko Braun <ike.braun@googlemail.com>
2013-01-22 11:13:45 +0100

    missing classes

--- gui/src/main/java/org/jboss/mbui/gui/behaviour/StatementEvent.java
+++ gui/src/main/java/org/jboss/mbui/gui/behaviour/StatementEvent.java
@@ -1,8 +1,61 @@
 package org.jboss.mbui.gui.behaviour;
 
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.HasHandlers;
+import org.jboss.mbui.model.structure.QName;
+
 /**
+ * A statement event alters the global dialog state.
+ *
  * @author Heiko Braun
- * @date 1/22/13
+ * @date 11/15/12
  */
-public class StatementEvent {
+public class StatementEvent extends GwtEvent<StatementEvent.Handler> {
+
+    public static final Type TYPE = new Type<Handler>();
+
+    private QName id;
+    private final String key;
+    private final String value;
+
+
+    public StatementEvent(QName id, String key, String value) {
+        super();
+        this.id = id;
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public QName getId() {
+        return id;
+    }
+
+    @Override
+    public Type<Handler> getAssociatedType() {
+        return TYPE;
+    }
+
+    @Override
+    protected void dispatch(Handler listener) {
+        if(listener.accepts(this))
+            listener.onStatementEvent(this);
+    }
+
+    public interface Handler extends EventHandler {
+        boolean accepts(StatementEvent event);
+        void onStatementEvent(StatementEvent event);
+    }
+
+    public static void fire(HasHandlers source, StatementEvent eventInstance) {
+        source.fireEvent(eventInstance);
+    }
 }
--- gui/src/main/java/org/jboss/mbui/model/behaviour/Statement.java
+++ gui/src/main/java/org/jboss/mbui/model/behaviour/Statement.java
@@ -1,8 +1,14 @@
 package org.jboss.mbui.model.behaviour;
 
+import org.jboss.mbui.model.structure.QName;
+
 /**
  * @author Heiko Braun
  * @date 1/22/13
  */
-public class Statement {
+public class Statement extends Transition {
+
+    public Statement(QName id) {
+        super(id);
+    }
 }
